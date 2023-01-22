54a66f933e6054e8c5dd1ae1b582f66181f32c63
geraskinalekseysergeevich <338892@niuitmo.ru>
2023-01-22 21:07:19 +0300

    Initial commit

---
+++ create.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<ul>
+		<li><a href="update.php">update</a></li>
+		<li><a href="delete.php">delete</a></li>
+	</ul>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<ul>
+		<li><a href="list.php">list</a></li>
+		<li><a href="update.php">update</a></li>
+		<li><a href="delete.php">delete</a></li>
+	</ul>
+</body>
+</html>
