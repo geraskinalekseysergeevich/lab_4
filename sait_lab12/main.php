32880f61f758008ff47be2e226abb6bd254ce249
geraskinalekseysergeevich <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2023-01-23 00:53:28 +0300

    Delete sait_lab12 directory

--- sait_lab12/main.php
+++
@@ -1,6 +0,0 @@
-<?php require 'header.php' ?>
-<?php require 'mainblock.php' ?>
-<?php require 'services.php' ?>
-<?php require 'shop.php'?>
-<?php require 'blog.php'?>
-<?php require 'footer.php' ?>
--- sait_lab12/script.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$xml = simplexml_load_file('db.xml');
-	$products = json_decode(json_encode($xml), true);
