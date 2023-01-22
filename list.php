628a77c08b8d18d06e96a17bc97d526106388762
geraskinalekseysergeevich <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2023-01-23 00:46:52 +0300

    Update list.php

--- list.php
+++ list.php
@@ -1,11 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<a href="create.php">create</a>
-</body>
-</html>
+<?php
+	$products = simplexml_load_file('products.xml');
+?>
+<table cellpadding="2" cellspacing="2" border="1">
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Price ($)</th>
+        <th>Description</th>
+        <th>Options</th>
+    </tr>
+    <?php foreach($products->product as $product) { ?>
+        <tr>
+            <td><?php echo $product['id']; ?></td>
+            <td><?php echo $product->name; ?></td>
+            <td><?php echo $product->price; ?></td>
+            <td><?php echo $product->description;?></td>
+            <td><a href="update.php?id=<?php echo $product['id']; ?>">Update</a>
+              </td>
+        </tr>
+    <?php } ?>
+    <a href="create.php?id=<?php echo $product['id']; ?>">Create</a><br>
+    <a href="index.php?id=<?php echo $product['id']; ?>">Index</a>
+</table>
