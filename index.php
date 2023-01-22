928d3d28de33c24d8c8d3b2756e06ac7011decae
geraskinalekseysergeevich <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2023-01-23 00:46:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,33 @@
 	<title>Document</title>
 </head>
 <body>
-	<ul>
-		<li><a href="list.php">list</a></li>
-		<li><a href="update.php">update</a></li>
-		<li><a href="delete.php">delete</a></li>
-	</ul>
+	<?php
+		$products = simplexml_load_file('products.xml');
+	?>
+	<table cellpadding="2" cellspacing="2" border="1">
+	    <tr>
+	        <th>Id</th>
+	        <th>Name</th>
+	        <th>Price</th>
+	        <th>Description</th>
+	        <th>Options</th>
+	    </tr>
+	    <?php foreach($products->product as $product) { ?>
+	        <tr>
+	            <td><?php echo $product['id']; ?></td>
+	            <td><?php echo $product->name; ?></td>
+	            <td><?php echo $product->price; ?></td>
+	            <td><?php echo $product->description;?></td>
+	            <td><a href="update.php?id=<?php echo $product->id; ?>">Update</a>
+	              </td>
+	        </tr>
+	    <?php } ?>
+	    <a href="update.php?id=<?php echo $product['id']; ?>">Update</a><br>
+	    <a href="list.php?id=<?php echo $product['id']; ?>">List</a><br>
+	    <form action="delete.php" method="post" name="delete">
+            <a href="delete.php?action=delete&id=<?php echo $product['id']; ?>"onclick="return confirm('Are you sure?')">Delete</a>
+        </form>
+
+	</table>
 </body>
 </html>
