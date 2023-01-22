f5311bade7676bd8a5ee9879f3416bcc4094b135
geraskinalekseysergeevich <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2023-01-23 00:44:41 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<ul>
-		<li><a href="index.php">back</a></li>
-	</ul>
-</body>
-</html>
+<?php
+if(isset($_GET['action'])) {
+    $products = simplexml_load_file('products.xml');
+    $id = $_GET['id'];
+    $index = 0;
+    $i = 0;
+    foreach($products->product as $product) {
+        if($product['id']==$id){
+            $index = $i;
+            break;
+        }
+        $i++;
+    }
+    unset($products->product[$index]);
+    file_put_contents('products.xml', $products->asXML());
+    header("location:list.php?id=".$id);
+}
+?>
+<a href="index.php?id=<?php echo $product['id']; ?>">Back</a>
